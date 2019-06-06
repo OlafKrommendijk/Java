30d980f41ce41463c2cb3e61364a7e2024a33010
Olaf Krommendijk <oh.krommendijk@student.alfa-college.nl>
2019-06-06 12:00:55 +0200

    quiz 4 afgerond

---
+++ quizmasters/src/hoofdstuk1/mijnKlasse.java
@@ -0,0 +1,18 @@
+package hoofdstuk1;
+
+public class mijnKlasse{
+	static int i1 = 7;
+	static int i2 = 12;
+	
+	public static void main(String[] args) {
+		i1 = 9;
+		i2 = 8;
+		
+		i1 = i1 - 3;
+		i2 = i2 + i1;
+		
+		System.out.print(i1 + ", ");
+		System.out.print(i2 + ", ");
+		
+	}
+}
