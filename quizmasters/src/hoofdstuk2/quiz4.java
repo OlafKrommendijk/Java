71dd248cdedffa648f0b45f7402e7b075f51233b
Olaf Krommendijk <oh.krommendijk@student.alfa-college.nl>
2019-06-06 14:07:27 +0200

    Quiz 4 van hoofdstuk 2 afgerond

---
+++ quizmasters/src/hoofdstuk2/quiz4.java
@@ -0,0 +1,26 @@
+package src.hoofdstuk2;
+
+public class quiz4{
+	public static void main(String[] args) {
+		int x = 15;
+		int y = 8;
+		int z = 3;
+		if(x == z) {
+			System.out.print("N");
+		}
+		if(x >= y) {
+			System.out.print("O");
+			System.out.print("Z");
+		}
+		if(x <= z) {
+			System.out.print("P");
+		}
+		if(z > y) {
+			System.out.print("Q");
+		}
+		if(y != z) {
+			System.out.print("R");
+		}
+		
+	}
+}
