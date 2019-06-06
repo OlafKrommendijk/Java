a52eef26166a9cfcc827412a505efad2d1264ac9
Olaf Krommendijk <oh.krommendijk@student.alfa-college.nl>
2019-06-06 11:49:38 +0200

    Quiz 2 afgerond

---
+++ quizmasters/src/hoofdstuk1/mijnVariabele.java
@@ -0,0 +1,19 @@
+package hoofdstuk1;
+
+public class mijnVariabele{
+	static byte b = 122;
+	static short s;
+	static float f1 = 3.50f;
+	static float f2 = 43.9f;
+	static double d =335.35;
+	static char mijnChar = 'Q';
+	
+	public static void main(String[] args) {
+		System.out.print(b + ", ");
+		System.out.print(s + ", ");
+		System.out.print(f1 + ", ");
+		System.out.print(f2 + ", ");
+		System.out.print(mijnChar + ", ");
+		System.out.print(d);
+	}
+}
