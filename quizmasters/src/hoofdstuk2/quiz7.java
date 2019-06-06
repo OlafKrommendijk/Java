fbfe7f927ab149e0e2fee6633174c41995b97f39
Olaf Krommendijk <oh.krommendijk@student.alfa-college.nl>
2019-06-06 14:07:44 +0200

    Quiz 7 van hoofdstuk 2 afgerond

---
+++ quizmasters/src/hoofdstuk2/quiz7.java
@@ -0,0 +1,17 @@
+package src.hoofdstuk2;
+
+public class quiz7{
+	public static void main(String[] args) {
+		int i1 = 3;
+		int i2 = 5;
+		int i3 = 12;
+		int i4 = 20;
+		
+		i1 += 4;
+		i2 *= 3;
+		i3 /= 3;
+		i4 -= 12;
+		
+		
+	}
+}
