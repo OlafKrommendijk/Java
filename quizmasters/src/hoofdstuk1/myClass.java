b0d5e5f44b60a5c344b6dda5149adae16c4ad9b4
Olaf Krommendijk <oh.krommendijk@student.alfa-college.nl>
2019-06-06 11:56:20 +0200

    Quiz nummer 2 afgerond

---
+++ quizmasters/src/hoofdstuk1/myClass.java
@@ -0,0 +1,23 @@
+package hoofdstuk1;
+
+public class myClass{
+	static int i;
+	static double d;
+	static boolean b;
+	
+	static double mijnVar = 1344.98;
+	static char mijnVar2 = 'g';
+	static int mijnVar3 = 766;
+	
+	
+	public static void main(String[] args) {
+		System.out.print(i + ", ");
+		System.out.print(d + ", ");
+		System.out.print(b + ", ");
+		
+		System.out.print(mijnVar + ", ");
+		System.out.print(mijnVar2 + ", ");
+		System.out.print(mijnVar3 + ", ");
+		
+	}
+}
