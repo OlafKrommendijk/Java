4d0eaa01eac58749d9a03cff1d9b763f4329dd1e
Olaf Krommendijk <oh.krommendijk@student.alfa-college.nl>
2019-06-09 13:17:47 +0200

    Quiz 1 van hoofdstuk 4 afgerond

---
+++ quizmasters/hoofdstuk4/quiz1.java
@@ -0,0 +1,20 @@
+package hoofdstuk4;
+
+public class quiz1{
+	public static void main(String[] args) {
+		int i = 4;
+		
+		while(i > 1) {
+			i --;
+			System.out.print(i);
+		}
+		
+	}
+}
+
+// als i gelijk is aan 3 is de output 21
+
+//als i gelijk is aan 1 is er geen output want hij komt de loop niet in
+//als i gelijk is aan 2 is de output 1
+//als i gelijk is aan 5 is de output 4321
+
