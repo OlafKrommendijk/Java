38bb9b69723a27d2ab90704efe623d951edb96b6
Olaf Krommendijk <oh.krommendijk@student.alfa-college.nl>
2019-06-09 13:42:50 +0200

    Quiz 7 van hoofdstuk 4 afgerond

---
+++ quizmasters/hoofdstuk4/quiz7.java
@@ -0,0 +1,21 @@
+package hoofdstuk4;
+
+public class quiz7{
+	public static void main(String[] args) {
+		char c = 'a';
+		char c2 = 'b';
+		
+		for(int i = 4; i >= 0; i --) {
+			if(i >= 2 || i == 4) {
+				System.out.print(c2);
+			}
+			else {
+				System.out.print(c);
+			}
+		}
+		
+	}
+}
+
+// er wordt bbbaa naar de uitvoer geschreven
+
