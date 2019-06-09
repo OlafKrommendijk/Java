cfff18f490aa58b0257c82a370c8211ccc4c4092
Olaf Krommendijk <oh.krommendijk@student.alfa-college.nl>
2019-06-09 13:53:50 +0200

    Quiz 9 van hoofdstuk 4 afgerond

---
+++ quizmasters/hoofdstuk4/quiz9.java
@@ -0,0 +1,15 @@
+package hoofdstuk4;
+
+public class quiz9{
+	public static void main(String[] args) {
+		for(int jaar = 2016; jaar <= 2040; jaar ++) {
+			if((jaar % 4 != 0)) {
+
+			}
+			System.out.print(jaar + " ");
+		}
+	}
+}
+
+// als je x naar 14 veranderd en y naar 5 wordt de uitvoer 25
+// als je break verwijderd wordt de uit voer 55
