5042e4c4f35d99d1a21d2924c66ba9f980d8c10f
Olaf Krommendijk <oh.krommendijk@student.alfa-college.nl>
2019-07-01 13:50:46 +0200

    Comments toegevoegd

--- quizmasters/hoofdstuk7/TekstManipulatie.java
+++ quizmasters/hoofdstuk7/TekstManipulatie.java
@@ -8,15 +8,23 @@ public class TekstManipulatie{
 	//maken van een method
 	public void stringDemo() {
 		System.out.print("Hoofdletters: " + zin.toUpperCase() + "\n");
+		//Zet de tekst naar hoofdletters
 		System.out.print("Kleine letters: " + zin.toLowerCase() + "\n");
+		//Zet de tekst naar kleine letters
 		System.out.print("Aantal tekens: " + zin.length() + "\n");
+		//Checkt de lengte van de tekst en laat zien hoeveel dit is
 		System.out.print("Verwijdert alle $: " + zin.replace("$", "") + "\n");
+		//haalt alle dollartekens uit de tekst
 		System.out.print("Eerste 10 letters weg: " + zin.substring(10) + "\n");
+		//Haalt de eerste 10 letters van de zin af
 		System.out.print("Index van het laatste dollar teken " + zin.indexOf("$") + "\n");
+		//kijkt waar het laatste dollar teken staat
 		System.out.print("Vervangt Brazilë door Argentinië: " + zin.replace("Brazilië", "Argentinië") + "\n");
+		//Vervangt het woord "Brazilië door Argentinië
 	}
 	
 	public static void main(String[] args) {
+		//uitvoeren van de method
 		TekstManipulatie tm = new TekstManipulatie();
 		tm.stringDemo();
 	}
