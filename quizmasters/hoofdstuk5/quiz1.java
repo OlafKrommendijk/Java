90912d22b228a726fee4c41b214a3631a32a8be9
Olaf Krommendijk <oh.krommendijk@student.alfa-college.nl>
2019-07-01 09:54:27 +0200

    Nu echt af, had te snel gelezen

--- quizmasters/hoofdstuk5/quiz1.java
+++ quizmasters/hoofdstuk5/quiz1.java
@@ -18,22 +18,34 @@ class Student{
 public class quiz1{
 	
 	public static void main(String[] args) {
-		//Maken van 2 nieuwe objecten
-		
+		//Maken van 2 nieuwe objecten (werknemers)
 		Werknemer wn = new Werknemer();
 		Werknemer wn2 = new Werknemer();
 		
+		//Maken van 2 nieuwe objecten (Studenten)
 		Student sd = new Student();
+		Student sd2 = new Student();
 		
+		//variabelen meegeven
 		wn.naam = "Jan";
 		wn.leeftijd = 20;
 		
+		sd2.naamStudent = "Jan";
+		sd2.leeftijdStudent = 33;
+		sd2.nummerStudent = "00383-384833";
+		sd2.woonplaatsStudent = "Brussel";
+		
+		//Uitvoer in de console
 		System.out.print(wn.naam + " ");
 		System.out.print(wn2.leeftijd + ", ");
 		
 		System.out.print(sd.naamStudent + " ");
 		System.out.print(sd.leeftijdStudent + " ");
 		System.out.print(sd.nummerStudent + " ");
-		System.out.print(sd.woonplaatsStudent + " ");
+		System.out.print(sd.woonplaatsStudent + ", ");
+		
+		System.out.print(sd2.naamStudent + " ");
+		System.out.print(sd2.nummerStudent + " ");
+		System.out.print(sd2.woonplaatsStudent + " ");
 	}
 }
