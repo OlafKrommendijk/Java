e21208f1914573022f6f0d6e0d5d004833b183fa
Olaf Krommendijk <oh.krommendijk@student.alfa-college.nl>
2019-07-02 11:50:01 +0200

    De eindopdracht van hoofdstuk 9 afgerond

---
+++ quizmasters/hoofdstuk9/Artikel.java
@@ -0,0 +1,16 @@
+package hoofdstuk9;
+
+public class Artikel{
+	//variabelen public declareren
+	public String naam;
+	public double prijs;
+	
+	//Constructor voor Artikel gedefineerd om namen en prijzen te initialiseren
+	public Artikel(String naam, double prijs){
+		this.naam = naam;
+		this.prijs = prijs;
+	}
+}
+
+
+	
