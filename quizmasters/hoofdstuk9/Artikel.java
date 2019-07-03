b69254e89d07a8508060f1204068fc63e9a172f4
Olaf Krommendijk <oh.krommendijk@student.alfa-college.nl>
2019-07-03 14:40:28 +0200

    Hoofdstuk 9 bekeken, eindopdracht van hoofdstuk 8 gemaakt.

--- quizmasters/hoofdstuk9/Artikel.java
+++ quizmasters/hoofdstuk9/Artikel.java
@@ -15,3 +15,31 @@ public class Artikel{
 
 
 	
+//9 door robin
+//import java.util.ArrayList;
+//
+//public class Winkelwagen
+//{
+//  private ArrayList<Artikel> artikelen = new ArrayList<Artikel>();
+//  public void printArtikelen()
+//  {
+//    for (int i = 0; i < artikelen.size(); i++)
+//    {
+//      int artikelId = i + 1;
+//      System.out.println("Artikel" + artikelId + ": " + artikelen.get(i).naam + ", Prijs: € " + artikelen.get(i).prijs);
+//    }
+//  }
+//  public static void main(String[] args)
+//  {
+//    Artikel artikel = new Artikel("Overhemd ", 20.39);
+//    Artikel artikel2 = new Artikel("Broek", 32.85);
+//    Artikel artikel3 = new Artikel("Sokken", 11.25);
+//    Artikel artikel4 = new Artikel("Jas ", 120.65);
+//    Winkelwagen sc = new Winkelwagen();
+//    sc.artikelen.add(artikel);
+//    sc.artikelen.add(artikel2);
+//    sc.artikelen.add(artikel3);
+//    sc.artikelen.add(artikel4);
+//    sc.printArtikelen();
+//  }
+//}
